07a335fa5641e08b8c8a5a4fda9dd5ec58e6acb8
Muhammad Sami <mohamedsami@edafa.com>
2018-11-26 15:24:32 +0200

    test2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,12 +13,10 @@ public class Greeter {
   }
   
   /**
-   * 
-   * @param someone
+   * @param someone test
    * @return greet string
    */
-
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
